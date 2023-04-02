db0a5524100ac3a5702d2fd2552060a51f86ce59
murillo <tumusx1@gmail.com>
2023-04-02 13:06:16 -0300

    feat: add alarmManager for devices idle

--- app/src/main/java/com/github/tumusx/camlocation/views/MainActivity.kt
+++ app/src/main/java/com/github/tumusx/camlocation/views/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        IdleAlarmManager(this).setExtractAlarmManager()
+        val idleAlarmManager = IdleAlarmManager(this)
+
+        idleAlarmManager.setAlarmManager()
     }
 }
--- idle/src/main/java/com/github/tumusx/idle/alarmManager/IdleMode.kt
+++ idle/src/main/java/com/github/tumusx/idle/alarmManager/IdleMode.kt
@@ -1,91 +1,66 @@
 package com.github.tumusx.idle.alarmManager
 
-import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
-import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import java.util.Calendar
+import com.github.tumusx.idle.R
+import java.util.*
 
-class IdleAlarmManager(private val context: Context) {
+class IdleAlarmManager(context: Context) {
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-    fun setExtractAlarmManager() {
-        val timeEvent = convertToAlarmTimeMillis(18, "08".toInt())
-        val pendingIntent = createIntentAlarm()
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, timeEvent, pendingIntent)
+    private val alarmIntent =
+        PendingIntent.getBroadcast(context, 0, Intent(context, AlarmReceiver::class.java), 0)
+    private val calendar = Calendar.getInstance().apply {
+        timeInMillis = System.currentTimeMillis()
+        set(Calendar.HOUR_OF_DAY, 13)
+        set(Calendar.MINUTE, 6)
     }
 
-    private fun createIntentAlarm() : PendingIntent {
-        // 1
-        val intent = Intent(context, IdleReceiver::class.java)
-        // 2
-        return PendingIntent.getBroadcast(
-            context,
-            REQUEST_CODE,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-    }
-
-    companion object {
-        const val REQUEST_CODE = 123
-    }
-}
-fun Calendar.setHourAndMinute(hour: Int, minute: Int) : Calendar {
-    set(Calendar.HOUR_OF_DAY, hour)
-    set(Calendar.MINUTE, minute)
-    set(Calendar.SECOND, 0)
-    set(Calendar.MILLISECOND, 0)
-    return this
-}
-fun Long.plusOneDay(): Long = this + 24 * 60 * 60 * 1000
-
-fun convertToAlarmTimeMillis(hours: Int, minute: Int) : Long {
-    val calendar = Calendar.getInstance()
-    val currentTime = calendar.timeInMillis
-    val preposedTime = calendar.setHourAndMinute(hours, minute).timeInMillis
-    val alarmTimeMillis = if(preposedTime > currentTime) {
-        preposedTime
-    }else{
-        preposedTime.plusOneDay()
-    }
-    return alarmTimeMillis
+    fun setAlarmManager() = alarmManager.setAndAllowWhileIdle (
+        AlarmManager.RTC_WAKEUP,
+        47040000,
+        alarmIntent
+    )
 }
 
-class IdleReceiver : BroadcastReceiver() {
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onReceive(context: Context, intent: Intent?) {
-        try {
-            val notification = NotificationCompat.Builder(context, "12345")
-                .setContentTitle("Hora de fazer checkout")
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setContentText("Entre no aplicativo para fazer checkout")
-                .build().also { notification ->
-                    if (ActivityCompat.checkSelfPermission(
-                            context,
-                            Manifest.permission.POST_NOTIFICATIONS
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        return
-                    }
-                    NotificationManagerCompat.from(context)
-                        .notify(notification.channelId.toInt(), notification)
-                }
-        } catch (exception: Exception) {
-            exception.printStackTrace()
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            var builder = NotificationCompat.Builder(context, "12345")
+                .setSmallIcon(R.drawable.notification)
+                .setContentTitle("Murillo")
+                .setContentText("Entre no nosso aplicativo")
+                .setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText("Entre no aplicativo para realizar suas tarefas...")
+                )
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            val name = "notificacao"
+            val description = "descricao"
+            val channelNotification = NotificationChannel(
+                "12345",
+                name,
+                NotificationManager.IMPORTANCE_HIGH,
+            ).also {
+                it.description = description
+            }
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channelNotification)
+            notificationManager.notify(12345, builder.build())
+            Log.d("ALARM", "ALARM TOCANDO")
+            Toast.makeText(context, "Alarme tocando", Toast.LENGTH_LONG).show()
         }
     }
 }
