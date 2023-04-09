47f672e67fda796f95342b2ec2642d3102e1c54a
murillo <tumusx1@gmail.com>
2023-04-09 16:12:33 -0300

    retirando receiver sms do manifest

--- app/src/main/java/com/github/tumusx/camlocation/views/MainActivity.kt
+++ app/src/main/java/com/github/tumusx/camlocation/views/MainActivity.kt
@@ -4,27 +4,43 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.provider.Telephony.Sms
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.tumusx.camlocation.R
 import com.github.tumusx.idle.alarmManager.IdleAlarmManager
 import com.github.tumusx.tasks.broadcast.SmsBroadcastReceiver
 import com.github.tumusx.tasks.listener.States
 import com.github.tumusx.tasks.util.TypeError
+import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val smsRetrievedAction = "com.google.android.gms.auth.api.phone.SMS_RETRIEVED"
-    }
+    private lateinit var smsClient: SmsRetrieverClient
 
-    private val broadcastReceiver = SmsBroadcastReceiver(this)
+    private val broadcastReceiver = SmsBroadcastReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        broadcastReceiver.activity = this
+        smsClient = SmsRetriever.getClient(this@MainActivity)
         val idleAlarmManager = IdleAlarmManager(this)
-        registerReceiver(broadcastReceiver, IntentFilter(smsRetrievedAction))
+        val actionId = IntentFilter()
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        registerReceiver(broadcastReceiver, intentFilter, SmsRetriever.SEND_PERMISSION,null)
+
+        configureFieldsWithSMS()
         idleAlarmManager.setAlarmManager()
+        initSmsClient()
     }
 
+    fun initSmsClient() {
+        smsClient.startSmsUserConsent(null).addOnSuccessListener {
+            Toast.makeText(this, "Esperando pelo SMS", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, it.message.toString(), Toast.LENGTH_LONG).show()
+            it.printStackTrace()
+        }
+    }
     private fun configureFieldsWithSMS() {
         States.instanceStates(object : States {
             override fun onSuccess(sms: String?) {
--- tasks/src/main/java/com/github/tumusx/tasks/broadcast/SmsBroadcastReceiver.kt
+++ tasks/src/main/java/com/github/tumusx/tasks/broadcast/SmsBroadcastReceiver.kt
@@ -5,16 +5,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.github.tumusx.tasks.listener.States
 import com.github.tumusx.tasks.util.TypeError
 import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
 
-class SmsBroadcastReceiver(private val activity: AppCompatActivity) : BroadcastReceiver() {
+class SmsBroadcastReceiver() : BroadcastReceiver() {
 
+    lateinit var activity: AppCompatActivity
     private fun <T>configureParcelable(parcelable: String, intent: Intent, classParcelable: Class<T>) : T? {
         return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.extras?.getParcelable(parcelable, classParcelable)
@@ -42,7 +45,7 @@ class SmsBroadcastReceiver(private val activity: AppCompatActivity) : BroadcastR
                         States.statesResult?.error(TypeError.ERROR)
                     }
 
-                    CommonStatusCodes.TIMEOUT -> {
+                    CommonStatusCodes.TIMEOUT -> {gi
                         States.statesResult?.error(TypeError.TIMEOUT)
                     }
                 }
